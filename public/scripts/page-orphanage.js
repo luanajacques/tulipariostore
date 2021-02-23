cbab213e87be13c356a2063611216a1edddb68de
luanajacques <74938095+luanajacques@users.noreply.github.com>
2021-02-23 17:42:10 -0300

    Add files via upload

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,40 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleclickzoom: false,
+    scrollwheelzoom: false,
+    zoomcontrol: false,
+}
+
+const map = L.map('mapid', options).setView([-27.2205871,-49.6544011], 15)
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+L
+.marker([-27.2205871,-49.6544011], {icon: icon})
+.addTo(map)
+
+function selectImage(event) {
+    const button = event.currentTarget
+
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button) {
+        button.classList.remove("active")
+    }
+
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    imageContainer.src = image.src
+
+    button.classList.add('active')
+}
